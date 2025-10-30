cea83792edfc48167b9698db705f57de9a4d08c8
Symphonic3 <46897919+Symphonic3@users.noreply.github.com>
2025-10-30 17:19:04 -0600

    Refactor peermanager

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { initPeersAsync } from './network/peermanager.js';
 
 async function bitcoin() {
-    await initPeersAsync(2);
+    await initPeersAsync(1);
 }
 
 const GENESIS_HASH = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f";
--- network/peermanager.js
+++ network/peermanager.js
@@ -1,23 +1,20 @@
 import { connectIPv4 } from "./peer.js";
 import { resolve4 } from 'dns/promises';
 
-const peers = [];
+const peers = new Set();
 
 export async function initPeersAsync(count) {
-    const offset = peers.length;
     for (let i = 0; i < count; i++) {
-        peers.push(await getPeerAsync(makeTerminator(offset + i)));
+        peers.add(await getPeerAsync());
     }
 }
 
-function makeTerminator(n) {
-    async function terminatorAsync() {
-        peers[n] = await getPeerAsync(terminatorAsync);
-    }
-    return terminatorAsync;
+async function terminatorAsync(peer) {
+    peers.delete(peer);
+    peers.add(await getPeerAsync());
 }
 
-async function getPeerAsync(terminatorAsync) {
+async function getPeerAsync() {
     const addresses = await resolve4("seed.btc.petertodd.org");
     const host = addresses[0]; //TODO replace this with a peerdb
     return connectIPv4(host, 8333, terminatorAsync);
