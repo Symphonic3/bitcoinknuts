4ab3211ebcc4f838717fd29fe48e42ac3e8b1b61
Symphonic3 <46897919+Symphonic3@users.noreply.github.com>
2025-10-30 20:02:18 -0600

    Fix line spacing

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import { Message, PROTOCOL_MESSAGE_TYPE } from './network/messages.js';
 import { getFirstPeer, initPeersAsync } from './network/peer.js';
+
 async function bitcoin() {
     await initPeersAsync(1);
 }
 
 bitcoin();
-
-
 
 const GENESIS_HASH = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f";
 
